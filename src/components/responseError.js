4e2255122965973c1b11ce048d0577efd4345f21
Narathorn Noophum <narathorn.work@gmail.com>
2024-03-12 08:36:09 +0700

    add ResponseError

---
+++ src/components/responseError.js
@@ -0,0 +1,10 @@
+const ResponseError = (error,res,) => {
+  console.error(error);
+  res.status(500).json({
+    status: 500,
+    code: "ERROR_INTERNAL_SERVER",
+    message: "Unknown Internal Server Error.",
+  });
+};
+
+export default ResponseError;
