6451271b1eb6c3a7ad909e67d39ced3cb739596c
Narathorn Noophum <narathorn.work@gmail.com>
2024-03-12 10:03:20 +0700

    fix component responseError

--- src/components/responseError.js
+++ src/components/responseError.js
@@ -2,7 +2,6 @@ const ResponseError = (error,res,) => {
   console.error(error);
   res.status(500).json({
     status: 500,
-    code: "ERROR_INTERNAL_SERVER",
     message: "Unknown Internal Server Error.",
   });
 };
