2a9f7d32a12066fc3284a418e38e90f0ac7deee0
Narathorn Noophum <narathorn.work@gmail.com>
2024-03-12 12:24:51 +0700

    add auth middleware

--- src/route/router.js
+++ src/route/router.js
@@ -1,15 +1,16 @@
-import { Router } from 'express'
-import { createUser, getUsers } from "../controller/userController.js"
-import { register, login } from "../controller/authController.js"
+import { Router } from "express";
+import Auth from "../middleware/auth.js";
+import { createUser, getUsers } from "../controller/userController.js";
+import { register, login } from "../controller/authController.js";
 
-const router = Router()
+const router = Router();
 
-router.route('/user')
+router.route("/user")
   .post(createUser)
-  .get(getUsers)
-router.route('/register')
-  .post(register)
-router.route('/login')
-  .post(login)
+  .get(Auth, getUsers);
+router.route("/register")
+  .post(register);  
+router.route("/login")
+  .post(login);
 
-export default router
+export default router;
